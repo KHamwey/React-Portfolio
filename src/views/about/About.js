bb833a9be42b96c7690d9d48e9365b1a676ead6a
Kyle Hamwey <kylehamwey2010@gmail.com>
2025-06-27 19:39:54 -0400

    resume

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -35,7 +35,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1zn3CkQ4bUp8LR0IZLOrR8210syt9wOiJ/viewS" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1zn3CkQ4bUp8LR0IZLOrR8210syt9wOiJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
